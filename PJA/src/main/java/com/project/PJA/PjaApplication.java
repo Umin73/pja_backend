b0ab47303d38d2decf646b0879fff50bf1f7e191
Umin73 <97716008+Umin73@users.noreply.github.com>
2025-05-26 10:00:15 +0900

    Create Project

---
+++ PJA/src/main/java/com/project/PJA/PjaApplication.java
@@ -0,0 +1,13 @@
+package com.project.PJA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PjaApplication.class, args);
+	}
+
+}
---
+++ PJA/src/test/java/com/project/PJA/PjaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.PJA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
