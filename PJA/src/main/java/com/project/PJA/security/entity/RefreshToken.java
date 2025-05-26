da60524cc0fe7a33303d07c849d28a2f93332489
Umin73 <97716008+Umin73@users.noreply.github.com>
2025-05-26 16:37:43 +0900

    feat: 회원관리 기본_ver2

--- PJA/src/main/java/com/project/PJA/security/entity/RefreshToken.java
+++ PJA/src/main/java/com/project/PJA/security/entity/RefreshToken.java
@@ -3,14 +3,11 @@ package com.project.PJA.security.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
-@Entity
-public class RefreshToken {
-    @Id
-    private String userId;
-
+public class RefreshToken implements Serializable {
+    private int userId;
     private String token;
-
     private LocalDateTime expiresAt;
 }
