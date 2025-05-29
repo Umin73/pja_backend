2d74e1d9836e2c1118df5639b4357f75e6b5f910
Umin73 <97716008+Umin73@users.noreply.github.com>
2025-05-29 10:35:03 +0900

    refactor: 토큰재발급, 로그아웃 수정

--- PJA/src/main/java/com/project/PJA/security/controller/AuthController.java
+++ PJA/src/main/java/com/project/PJA/security/controller/AuthController.java
@@ -1,9 +1,12 @@
 package com.project.PJA.security.controller;
 
+import com.project.PJA.common.dto.ErrorResponse;
 import com.project.PJA.common.dto.SuccessResponse;
 import com.project.PJA.security.dto.LoginDto;
+import com.project.PJA.security.dto.TokenRequestDto;
 import com.project.PJA.security.jwt.JwtTokenProvider;
 import com.project.PJA.security.service.AuthUserService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -11,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.*;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.ErrorResponseException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -22,42 +26,29 @@ import java.util.concurrent.TimeUnit;
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final JwtTokenProvider jwtTokenProvider;
-    private final UserDetailsService userDetailsService;
-    private final StringRedisTemplate redisTemplate;
     private final AuthUserService authUserService;
 
     @PostMapping("/login")
     public SuccessResponse<?> login(@RequestBody LoginDto loginDto) {
         log.info("== 로그인 API 진입 == UID: {}", loginDto.getUid());
-        try {
-            Map<Object, Object> tokens = authUserService.login(loginDto);
-            return new SuccessResponse<>("success", "로그인에 성공하였습니다", tokens);
-        } catch (Exception e) {
-            log.error("로그인 중 예외 발생", e);
-            return new SuccessResponse<>("fail", "로그인 중 오류 발생", null);
-        }
+        Map<Object, Object> tokens = authUserService.login(loginDto);
+        return new SuccessResponse<>("success", "로그인에 성공하였습니다", tokens);
     }
 
 
     @PostMapping("/reissue")
-    public ResponseEntity<?> reissue(@RequestParam String refreshToken) {
-        if (!jwtTokenProvider.validateToken(refreshToken)) return ResponseEntity.status(401).build();
-
-        String uid = jwtTokenProvider.getUid(refreshToken);
-        String storedToken = redisTemplate.opsForValue().get("RT:" + uid);
-
-        if (storedToken == null || !storedToken.equals(refreshToken)) return ResponseEntity.status(401).build();
-
-        UserDetails user = userDetailsService.loadUserByUsername(uid);
-        String newAccessToken = jwtTokenProvider.createToken(uid, user.getAuthorities().iterator().next().getAuthority());
-
-        return ResponseEntity.ok().body(Map.of("accessToken", newAccessToken));
+    public SuccessResponse<?> reissue(@RequestBody TokenRequestDto tokenRequestDto) {
+        log.info("== 토큰 재발급 API 진입 == RT: {}", tokenRequestDto.getRefreshToken());
+        String newAccessToken = authUserService.reissue(tokenRequestDto.getRefreshToken());
+        return new SuccessResponse<>("success", "토큰 재발급에 성공하였습니다.", Map.of("accessToken", newAccessToken));
     }
 
     @PostMapping("/logout")
-    public ResponseEntity<?> logout(@RequestParam String uid) {
-        redisTemplate.delete("RT:" + uid);
-        return ResponseEntity.ok().body(Map.of("message", "로그아웃에 성공하였습니다."));
+    public SuccessResponse<?> logout(HttpServletRequest request, @RequestBody String uid) {
+        if(authUserService.logout(request, uid)) {
+            return new SuccessResponse<>("success", "로그아웃에 성공하였습니다.", null);
+        } else {
+            throw new RuntimeException("로그아웃에 실패하였습니다.");
+        }
     }
 }
--- PJA/src/main/java/com/project/PJA/security/service/AuthUserService.java
+++ PJA/src/main/java/com/project/PJA/security/service/AuthUserService.java
@@ -3,6 +3,7 @@ package com.project.PJA.security.service;
 import com.project.PJA.exception.UnauthorizedException;
 import com.project.PJA.security.dto.LoginDto;
 import com.project.PJA.security.jwt.JwtTokenProvider;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -10,6 +11,7 @@ import org.springframework.security.authentication.*;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -49,4 +51,39 @@ public class AuthUserService {
         return Map.of("accessToken", accessToken, "refreshToken", refreshToken);
     }
 
+    public String reissue(String rt) {
+        if(!jwtTokenProvider.validateToken(rt)) {
+            log.info("RT 유효하지 않음 -- (1)");
+            throw new UnauthorizedException("Refresh Token이 유효하지 않거나 만료되었습니다.");
+        }
+
+        String uid = jwtTokenProvider.getUid(rt);
+        log.info("uid: {}", uid);
+        String storedToken = redisTemplate.opsForValue().get("RT:"+uid);
+        log.info("storedToken: {}", storedToken);
+
+        if(storedToken == null || !storedToken.equals(rt)) {
+            log.info("RT 유효하지 않음 -- (2)");
+            throw new UnauthorizedException("Refresh Token이 유효하지 않거나 만료되었습니다.");
+        }
+
+        UserDetails user = userDetailsService.loadUserByUsername(uid);
+        log.info("user: {}", user);
+        String newAccessToken = jwtTokenProvider.createToken(uid, user.getAuthorities().iterator().next().getAuthority());
+
+        return newAccessToken;
+    }
+
+    public boolean logout(HttpServletRequest request, String uid) {
+        String AT = jwtTokenProvider.resolveToken(request);
+        long expiration = jwtTokenProvider.getExpiration(AT);
+
+        // Redis에 블랙리스트로 등록
+        redisTemplate.opsForValue().set("BL:" + AT, "logout", expiration, TimeUnit.MILLISECONDS);
+
+        // Refresh Token 삭제
+        redisTemplate.delete("RT:" + uid);
+
+        return true;
+    }
 }
