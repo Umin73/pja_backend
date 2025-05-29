d624fe786410fd403a6c82778def8cc5dc801fa7
Umin73 <97716008+Umin73@users.noreply.github.com>
2025-05-29 11:26:13 +0900

    feat: 아이디 찾기 기능 추가

--- PJA/src/main/java/com/project/PJA/user/service/UserService.java
+++ PJA/src/main/java/com/project/PJA/user/service/UserService.java
@@ -1,17 +1,25 @@
 package com.project.PJA.user.service;
 
+import com.project.PJA.exception.NotFoundException;
 import com.project.PJA.user.dto.SignupDto;
 import com.project.PJA.user.entity.UserRole;
 import com.project.PJA.user.entity.Users;
 import com.project.PJA.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
+    private static final Logger log = LoggerFactory.getLogger(UserService.class);
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
@@ -27,5 +35,20 @@ public class UserService {
         userRepository.save(user);
 
         return true;
+    }
+
+    public Map<String, String> findId(String email) {
+        Optional<Users> optionalUsers = userRepository.findByEmail(email);
+        log.info("OptionalUsers: {}", optionalUsers);
+        if (optionalUsers.isPresent()) {
+            Users users = optionalUsers.get();
+
+            Map<String, String> map = new HashMap<>();
+            map.put("uid", users.getUid());
+
+            return map;
+        } else {
+            throw new NotFoundException("일치하는 사용자를 찾을 수 없습니다.");
+        }
     }
 }
