ca6a6e58f9ad07dc6a8e95c6c019861be838972d
yeongchul <revan2015@naver.com>
2025-05-30 18:04:17 +0900

    feat: 사용자에게 이메일 전송 기능 구현
    사용자에게 이메일 전송 기능 구현

---
+++ PJA/src/main/java/com/project/PJA/email/service/EmailService.java
@@ -0,0 +1,6 @@
+package com.project.PJA.email.service;
+
+public interface EmailService {
+    void sendInvitationEmail(String to, String inviteUrl);
+    void sendVerificationCode(String to, String code);
+}
