a62c8f6c5a5283e1244ceee597659aa8cb2be5a6
yeongchul <revan2015@naver.com>
2025-06-05 17:54:39 +0900

    refactor: findByWorkspace_WorkspaceId 데이터 타입 변경
    findByWorkspace_WorkspaceId 데이터 타입 변경

--- PJA/src/main/java/com/project/PJA/idea/repository/IdeaRepository.java
+++ PJA/src/main/java/com/project/PJA/idea/repository/IdeaRepository.java
@@ -3,6 +3,8 @@ package com.project.PJA.idea.repository;
 import com.project.PJA.idea.entity.Idea;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface IdeaRepository extends JpaRepository<Idea, Long> {
-    Idea findByWorkspaceId(Long workspaceId);
+    Optional<Idea> findByWorkspace_WorkspaceId(Long workspaceId);
 }
