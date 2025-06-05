5f652b3a9baf53ea062b57769f115b729a7418e4
yeongchul <revan2015@naver.com>
2025-06-05 17:52:16 +0900

    chore: ProblemSolving 주석 삭제
    ProblemSolving 주석 삭제

--- PJA/src/main/java/com/project/PJA/idea/dto/ProblemSolving.java
+++ PJA/src/main/java/com/project/PJA/idea/dto/ProblemSolving.java
@@ -1,24 +1,21 @@
 package com.project.PJA.idea.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.List;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class ProblemSolving {
-    @JsonProperty("current_problem")
+    //@JsonProperty("current_problem")
     private String currentProblem;
 
-    @JsonProperty("solution_idea")
+    //@JsonProperty("solution_idea")
     private String solutionIdea;
 
-    @JsonProperty("expected_benefits")
+    //@JsonProperty("expected_benefits")
     private List<String> expectedBenefits;
 }
