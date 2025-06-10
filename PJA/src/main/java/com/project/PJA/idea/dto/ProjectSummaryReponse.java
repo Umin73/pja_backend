101e181ee3e417a16cfacba061d198d8746e58fc
yeongchul <revan2015@naver.com>
2025-06-10 13:30:36 +0900

    refactor: 아이디어 요약 응답에 workspaceId 삭제
    아이디어 요약 응답에 workspaceId 삭제

--- PJA/src/main/java/com/project/PJA/idea/dto/ProjectSummaryReponse.java
+++ PJA/src/main/java/com/project/PJA/idea/dto/ProjectSummaryReponse.java
@@ -11,7 +11,6 @@ import java.util.List;
 @AllArgsConstructor
 public class ProjectSummaryReponse {
     private Long ideaId;
-    private Long workspaceId;
     private String title;
     private String category;
     private List<String> targetUsers;
