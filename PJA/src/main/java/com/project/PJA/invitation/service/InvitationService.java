9fae2f4a288c26eb0de62aa8bd38b2407653a0a8
yeongchul <revan2015@naver.com>
2025-06-15 23:46:32 +0900

    refactor: 초대 응답에서 workspaceId 삭제
    초대 응답에서 workspaceId 삭제

--- PJA/src/main/java/com/project/PJA/invitation/service/InvitationService.java
+++ PJA/src/main/java/com/project/PJA/invitation/service/InvitationService.java
@@ -63,7 +63,7 @@ public class InvitationService {
             emailService.sendInvitationEmail(invitation.getInvitedEmail(), inviteUrl);
         }
 
-        return new WorkspaceInviteResponse(workspaceId, request.getEmails(), request.getRole());
+        return new WorkspaceInviteResponse(request.getEmails(), request.getRole());
     }
 
     // 초대 링크 정보 조회
