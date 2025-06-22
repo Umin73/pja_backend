2a9d87a1cba2ed2b3485062b365828c37aa7f838
Umin73 <97716008+Umin73@users.noreply.github.com>
2025-06-22 21:15:28 +0900

    fix: json 파싱 시 이름 매칭 문제 해결

--- PJA/src/main/java/com/project/PJA/user_act_log/dto/DetailsParticipants.java
+++ PJA/src/main/java/com/project/PJA/user_act_log/dto/DetailsParticipants.java
@@ -1,5 +1,6 @@
 package com.project.PJA.user_act_log.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,6 +11,8 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class DetailsParticipants {
+    @JsonProperty("userId")
     private Integer userId;
-    private String userName;
+    @JsonProperty("username")
+    private String username;
 }
