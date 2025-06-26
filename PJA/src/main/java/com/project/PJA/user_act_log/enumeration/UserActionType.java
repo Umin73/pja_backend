ec4e3a69ef667d0ddf99599c97b9b607ed476a62
Umin73 <97716008+Umin73@users.noreply.github.com>
2025-06-26 16:16:34 +0900

    feat: UserActionType 일부 추가
    추가 항목
    - UPDATE_PROJECT_PROGRESS_ACTION, // 액션 수정 시
    - UPDATE_IMPORTANCE, // 중요도 수정 시

--- PJA/src/main/java/com/project/PJA/user_act_log/enumeration/UserActionType.java
+++ PJA/src/main/java/com/project/PJA/user_act_log/enumeration/UserActionType.java
@@ -1,10 +1,10 @@
 package com.project.PJA.user_act_log.enumeration;
 
 public enum UserActionType {
-    UPDATE_PARTICIPANT_TO_ACTION, // 참여자 추가 시
+    UPDATE_PARTICIPANT_TO_ACTION, // 참여자 추가(수정) 시
     CREATE_PROJECT_PROGRESS_ACTION, // 액션 생성 시
     DELETE_PROJECT_PROGRESS_ACTION, // 액션 삭제 시
     DONE_PROJECT_PROGRESS_ACTION, // 액션 완료 시
-//    UPDATE_PROJECT_PROGRESS_ACTION,
-//    UPDATE_IMPORTANCE,
+    UPDATE_PROJECT_PROGRESS_ACTION, // 액션 수정 시
+    UPDATE_IMPORTANCE, // 중요도 수정 시
 }
