e79db2701058a43982fa2e7ab4dd96318f444541
yeongchul <revan2015@naver.com>
2025-06-30 13:35:41 +0900

    refactor: 아이디어 입력 수정 필드별로 수정되게 수정

---
+++ PJA/src/main/java/com/project/PJA/ideainput/dto/MainFunctionRequest.java
@@ -0,0 +1,12 @@
+package com.project.PJA.ideainput.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MainFunctionRequest {
+    private String content;
+}
---
+++ PJA/src/main/java/com/project/PJA/ideainput/dto/ProjectDescriptionResponse.java
@@ -0,0 +1,13 @@
+package com.project.PJA.ideainput.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProjectDescriptionResponse {
+    private Long ideaInputId;
+    private String projectDescription;
+}
